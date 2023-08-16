fe225af1ab5b06a7568e551db6bad7d4d27cb49c
Ayşe Dilara Öztürk <adilaraozturk@gmail.com>
2023-08-16 14:26:22 +0300

    bazı sorgu komutlarını öğrendik

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',function(){
+    /*
+    $users = DB::table('users)->pluck('name); //*pluck ile spesifik bir alanın arrayını alabiliriz.
+    return $users;
+    */
+
+    /*
+    $users = DB::table('users)->where('name', 'Ayşe Dilara Öztürk')->value('name'); //*value ile spesifik bir değeri alabiliriz.
+    return $users;
+    */
+
+    /*
+    $users = DB::table('users)->where('id', 5)->value('email'); //*value ile spesifik bir değeri alabiliriz, burada id'si 5 olan değerin email adresini aldık. 
+    return $users;
+    */
+
+    /*
+    $users = DB::table('users)->where('id', 1)->first(); //*first ile spesifik bir alanın bütün değerlerini alabiliriz.
+    return $users;
+    */
+
+    /*
+    $users = DB::table('users)->find('name'); //*find ile spesifik bir alanı bulur ve bütün değerlerini getirir.
+    return $users;
+    */
+
+    /*
+    $users = DB::table('users')->select('name as Adı')->get(); //*select ile bir tablodaki sütun ismini bulur, onu yeni bir isimle tarayıcıya yazdırır ve bütün isim değerlerini getirir.
+    return $users;
+    */
+
 });
